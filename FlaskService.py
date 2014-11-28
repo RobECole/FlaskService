906af08e41a3b75dc500a5b17b73def1eb2f066f
Rob <Rob.eric.cole@gmail.com>
2014-11-28 18:39:38 -0500

    Added connection to database, successfully making free champ query and returning json files

--- FlaskService.py
+++ FlaskService.py
@@ -1,30 +1,39 @@
-from flask import Flask, jsonify, abort
-
+from flask import Flask
+import json
+import psycopg2
 app = Flask(__name__)
 
 tasks = [
-        {
-          "description": "Milk, Cheese, Pizza, Fruit, Tylenol",
-          "done": False,
-          "id": 1,
-          "title": "Buy groceries"
-        },
-        {
-          "description": "Need to find a good Python tutorial on the web",
-          "done": False,
-          "id": 2,
-          "title": "Learn Python"
-        }
-        #added test
+    {
+        "description": "Milk, Cheese, Pizza, Fruit, Tylenol",
+        "done": False,
+        "id": 1,
+        "title": "Buy groceries"
+    },
+    {
+        'id': 2,
+        'title': 'Learn Python',
+        'description': 'Need to find a good Python tutorial on the web',
+        'done': False
+    }
 ]
+@app.route('/api/test', methods=['GET'])
+def test():
+    return json.dumps(tasks)
 
-@app.route('/query/<int:task_id>', methods=['GET'])
-def get_tasks(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'tasks': task[0]})
+@app.route('/api/freeChamps', methods=['GET'])
+def freechamps():
+    connect.cursor.execute("SELECT CN.Name FROM LEAGUE.CHAMPION AS C RIGHT JOIN LEAGUE.CHAMPNAME AS CN ON CN.ID = C.Champ_id WHERE Free_to_play = 'true'")
+    record = connect.cursor.fetchall()
+    print record
+    return json.dumps(record)
 
+
+class connect():
+
+    conn_string = "host='localhost' dbname='league_circuit' user='postgres' password='testdb'"
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5001, debug=True)
